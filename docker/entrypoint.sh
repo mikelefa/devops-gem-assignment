9c4f1e933e0922f32d641dd22325c7ece23ad904
Mikele Fakadu <mikelefa@users.noreply.github.com>
2025-04-25 12:04:26 -0400

    Add entry point for docker container in pipeline

---
+++ docker/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Start Xvfb for headless GUI rendering
+Xvfb :99 -screen 0 1280x1024x24 &
+
+export DISPLAY=:99
+source /opt/ros/noetic/setup.bash
+source /ros_ws/devel/setup.bash
