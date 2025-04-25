b1f34d3353ef4875db0241c0aacda64d5fe38948
Mikele Fakadu <mikelefa@users.noreply.github.com>
2025-04-25 12:32:01 -0400

    Add waits for docker to start up

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Start Xvfb for headless GUI rendering
 Xvfb :99 -screen 0 1280x1024x24 &
-
 export DISPLAY=:99
 source /opt/ros/noetic/setup.bash
 source /ros_ws/devel/setup.bash
+roslaunch gem_gazebo gem_gazebo_rviz.launch gui:=false use_rviz:=false
